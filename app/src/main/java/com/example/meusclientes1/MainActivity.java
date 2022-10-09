5e5431646ffb74f7d1919c26427e5517a90ca0e7
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2022-10-09 20:28:42 -0300

    First commit

--- app/src/main/java/com/example/meusclientes1/MainActivity.java
+++ app/src/main/java/com/example/meusclientes1/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.meusclientes1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
     }
 }
