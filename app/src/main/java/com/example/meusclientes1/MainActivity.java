561c32d17c58219b793959f3bfeeda69c5b82a1f
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2022-10-11 11:37:41 -0300

    92 - curso

--- app/src/main/java/com/example/meusclientes1/MainActivity.java
+++ app/src/main/java/com/example/meusclientes1/MainActivity.java
@@ -2,22 +2,43 @@ package com.example.meusclientes1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    // Gerenciamente dos fragmentos
+    FragmentManager fragmentManager;
+
+    // Gerenciamento do menu drawer
+    NavigationView navigationView;
+
+    // Gereciamento do menu action bar
+    Menu menu;
+
+    // Gerenciamento de cada item do menu drawer
+    MenuItem nav_preto;
+    MenuItem nav_vermelho;
+    MenuItem nav_azul;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         return false;
     }
+
 }
