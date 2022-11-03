50b069c06dde693422c0ecaf1ca9835ea96dabb2
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2022-11-03 10:56:09 -0300

    Criando o evento para salvar dados no banco de dados

--- app/src/main/java/com/example/meusclientes1/view/AdicionarClienteFragmento.java
+++ app/src/main/java/com/example/meusclientes1/view/AdicionarClienteFragmento.java
@@ -13,13 +13,14 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.meusclientes1.R;
 import com.example.meusclientes1.controller.ClienteController;
 import com.example.meusclientes1.model.Cliente;
 
-public class AdicionarClienteFragmento extends Fragment {
+public class AdicionarClienteFragmento extends Fragment{
 
     View view;
 
@@ -35,12 +36,13 @@ public class AdicionarClienteFragmento extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(@Nullable LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
 
         initComponents();
 
@@ -69,8 +71,8 @@ public class AdicionarClienteFragmento extends Fragment {
         cancelar = view.findViewById(R.id.btnCancelar);
         salvar = view.findViewById(R.id.btnSalvar);
 
-        //novoCliente = new Cliente();
-        //clienteController = new ClienteController(getContext());
+        novoCliente = new Cliente();
+        clienteController = new ClienteController(getContext());
 
     }
 
@@ -99,8 +101,57 @@ public class AdicionarClienteFragmento extends Fragment {
                     nome.requestFocus();
                     isDadosOK = false;
                 }
+                if(TextUtils.isEmpty(telefone.getText().toString())){
+                    nome.setError("Digite o telefone !");
+                    nome.requestFocus();
+                    isDadosOK = false;
+                }
+                if(TextUtils.isEmpty(email.getText().toString())){
+                    nome.setError("Digite o email!");
+                    nome.requestFocus();
+                    isDadosOK = false;
+                }
+                if(TextUtils.isEmpty(cep.getText().toString())){
+                    nome.setError("Digite o CEP!");
+                    nome.requestFocus();
+                    isDadosOK = false;
+                }
+                if(TextUtils.isEmpty(logradouro.getText().toString())){
+                    nome.setError("Digite o logradouro!");
+                    nome.requestFocus();
+                    isDadosOK = false;
+                }
+                if(TextUtils.isEmpty(numero.getText().toString())){
+                    nome.setError("Digite o número!");
+                    nome.requestFocus();
+                    isDadosOK = false;
+                }
+                if(TextUtils.isEmpty(bairro.getText().toString())){
+                    nome.setError("Digite o bairro!");
+                    nome.requestFocus();
+                    isDadosOK = false;
+                }
+                if(TextUtils.isEmpty(cidade.getText().toString())){
+                    nome.setError("Digite a cidade!");
+                    nome.requestFocus();
+                    isDadosOK = false;
+                }
+                if(TextUtils.isEmpty(estado.getText().toString())){
+                    nome.setError("Digite o estado!");
+                    nome.requestFocus();
+                    isDadosOK = false;
+                }
+
 
                 if(isDadosOK){
+
+                    novoCliente.setNome(nome.getText().toString());
+                    novoCliente.setTelefone(telefone.getText().toString());
+                    novoCliente.setEmail(email.getText().toString());
+                    // Cast
+                    novoCliente.setCep(Integer.parseInt(cep.getText().toString()));
+                    novoCliente.setLogradouro(logradouro.getText().toString());
+
                     //clienteController.incluir(novoCliente);
                 } else {
                     // notificar o usuário
